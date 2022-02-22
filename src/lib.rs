a7863c1525d9301447b982dce2b7e150e82def36
deemerdev <88148874+arkateFE@users.noreply.github.com>
2022-02-22 14:27:15 +0300

    q
    q

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,11 @@ use std::io::*;
 
 
 
-pub fn gen_rand(){
+pub fn gen_rand(a:usize){
     let code = "+0 000 ";//enter country and region code here
     let path = "generated.txt";
     let mut f = File::create(path).expect("err creating");
-     for _ in 0..100{
+     for _ in 0..a/2{
 
         let otp = format!("{:?}",rand::thread_rng().gen_range(0.0..1.0));
         let frmt = &otp[2..9];
